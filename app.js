b30dc9d6477f8da9ef80f2bedfb8710419893fd1
memset0 <34177126+memset0@users.noreply.github.com>
2023-01-15 11:41:41 +0800

    feat: allow cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv/config');
 const express = require('express');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const Debug = require('debug');
 const githubCdnRouter = require('.');
@@ -8,6 +9,7 @@ const debug = Debug('github-cdn');
 const { port = 3005 } = require('./config');
 
 const app = express();
+app.use(cors());
 app.disable('x-powered-by');
 app.use(cookieParser());
 app.use(githubCdnRouter());
